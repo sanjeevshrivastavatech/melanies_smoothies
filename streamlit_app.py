9b6e9461894ba8c1c20a7f0b7edd0175d7201fa2
sanjeevshrivastavatech <76872263+sanjeevshrivastavatech@users.noreply.github.com>
2026-06-27 23:27:42 -0400

    Fix formatting and add dataframe display

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
@@ -52,7 +52,8 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values('""" + ingredients_string+ """','"""+name_on_order+"""')"""
     #st.write(my_insert_stmt)
-    #st.stop()
+    st.dataframe(data=my_dataframe, use_container_width=True)
+    st.stop()
 
     time_to_insert = st.button('Submit Order')    
 
